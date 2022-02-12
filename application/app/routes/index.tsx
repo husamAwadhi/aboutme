6f9c6b96c489dafd42814e0037712daac29a4b6d
Husam <husam.awadhi@gmail.com>
2022-02-12 15:19:17 +0800

    minor fixes/changes for docker implementation

--- application/app/routes/index.tsx
+++ application/app/routes/index.tsx
@@ -1,32 +1,11 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>We'll be back soon!</h1>
+      <div>
+          <p>Sorry for the inconvenience but we're performing some maintenance at the moment. If you need to you can always <a href="mailto:husam.awadhi@gmail.com">contact us</a>, otherwise we'll be back online shortly!</p>
+          <p> - Husam A</p>
+      </div>
     </div>
   );
 }
